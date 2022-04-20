54c38d4786decfc3b4f7d5b64ea451b81ff104b0
Sultan Fariz <sultan_fariz@apps.ipb.ac.id>
2022-04-20 14:09:42 +0700

    feat: add repositories for thread database layer

---
+++ src/Infrastructures/repository/ThreadRepositoryPostgres.js
@@ -0,0 +1,42 @@
+const InvariantError = require('../../Commons/exceptions/InvariantError');
+const AddedThread = require('../../Domains/threads/entities/AddedThread');
+const ThreadRepository = require('../../Domains/threads/ThreadRepository');
+
+class ThreadRepositoryPostgres extends ThreadRepository {
+  constructor(pool, idGenerator) {
+    super();
+    this._pool = pool;
+    this._idGenerator = idGenerator;
+  }
+
+  async insertThread(addThread) {
+    const { title, body, owner_id } = addThread;
+    const id = `thread-${this._idGenerator()}`;
+
+    const query = {
+      text: 'INSERT INTO threads VALUES($1, $2, $3, $4) RETURNING id, title, body, owner_id, created_at',
+      values: [id, owner_id, title, body],
+    };
+
+    const result = await this._pool.query(query);
+
+    return new AddedThread({ ...result.rows[0] });
+  }
+
+  async getThreadById(id) {
+    const query = {
+      text: 'SELECT * FROM threads WHERE id = $1',
+      values: [id],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rowCount) {
+      throw new InvariantError('thread tidak ditemukan');
+    }
+
+    return new AddedThread({ ...result.rows[0] });
+  }
+}
+
+module.exports = ThreadRepositoryPostgres;
