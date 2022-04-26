a615895b08990e901341bc56bed99510f6dc0d68
Sultan Fariz <sultan_fariz@apps.ipb.ac.id>
2022-04-26 14:00:27 +0700

    test: add unit test for comment repository interface

---
+++ src/Domains/comments/_test/CommentRepository.test.js
@@ -0,0 +1,12 @@
+const CommentRepository = require('../CommentRepository');
+
+describe('CommentRepository interface', () => {
+  it('should throw error when invoke abstract behavior', async () => {
+    // Arrange
+    const commentRepository = new CommentRepository();
+
+    // Action and Assert
+    await expect(commentRepository.insertComment({})).rejects.toThrowError('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+    await expect(commentRepository.deleteComment('comment-123')).rejects.toThrowError('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  });
+});
