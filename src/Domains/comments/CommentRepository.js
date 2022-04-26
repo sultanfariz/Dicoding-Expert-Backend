8ab397f575bc789981e2f79a614ff8262c5801e7
Sultan Fariz <sultan_fariz@apps.ipb.ac.id>
2022-04-26 14:05:01 +0700

    feat: add comment repository interface

---
+++ src/Domains/comments/CommentRepository.js
@@ -0,0 +1,11 @@
+class CommentRepository {
+  async insertComment(comment) {
+    throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+
+  async deleteComment(id) {
+    throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+}
+
+module.exports = CommentRepository;
