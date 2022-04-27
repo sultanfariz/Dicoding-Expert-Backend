ecc465e81cc38e1028adc063d56bfbb3cf60e8fe
Sultan Fariz <sultan_fariz@apps.ipb.ac.id>
2022-04-27 15:12:32 +0700

    feat: add comment repository layer

---
+++ src/Infrastructures/repository/CommentRepositoryPostgres.js
@@ -0,0 +1,44 @@
+const InvariantError = require('../../Commons/exceptions/InvariantError');
+const AddedComment = require('../../Domains/comments/entities/AddedComment');
+const CommentRepository = require('../../Domains/comments/CommentRepository');
+
+class CommentRepositoryPostgres extends CommentRepository {
+  constructor(pool, idGenerator) {
+    super();
+    this._pool = pool;
+    this._idGenerator = idGenerator;
+  }
+
+  async insertComment(addComment) {
+    const { content, owner_id, thread_id } = addComment;
+    const id = `comment-${this._idGenerator()}`;
+
+    console.log("============================MASUK SINI 1============================");
+    console.log("addComment: ", addComment);
+    const query = {
+      text: 'INSERT INTO comments VALUES($1, $2, $3, $4) RETURNING id, thread_id, owner_id, content, created_at',
+      values: [id, thread_id, owner_id, content],
+    };
+
+    const result = await this._pool.query(query);
+    console.log("result", result);
+    console.log("============================MASUK SINI 2============================");
+
+    return new AddedComment({ ...result.rows[0] });
+  }
+
+  async deleteComment(id) {
+    const query = {
+      text: 'UPDATE comments SET deleted_at = NOW() WHERE id = $1',
+      values: [id],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rowCount) {
+      throw new InvariantError('comment tidak ditemukan');
+    }
+  }
+}
+
+module.exports = CommentRepositoryPostgres;
