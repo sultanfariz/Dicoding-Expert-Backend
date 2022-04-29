9f2bb69e68476c6040908bbbc6bc84ecd2de70fd
Sultan Fariz <sultan_fariz@apps.ipb.ac.id>
2022-04-29 09:39:44 +0700

    test: add end to end test for thread domain

---
+++ src/Infrastructures/http/_test/threads.test.js
@@ -0,0 +1,106 @@
+const pool = require('../../database/postgres/pool');
+const ThreadsTableTestHelper = require('../../../../tests/ThreadsTableTestHelper');
+const UsersTableTestHelper = require('../../../../tests/UsersTableTestHelper');
+const AuthenticationsTestHelper = require('../../../../tests/AuthenticationsTestHelper');
+const container = require('../../container');
+const createServer = require('../createServer');
+
+describe('/threads endpoint', () => {
+  afterAll(async () => {
+    await pool.end();
+  });
+
+  afterEach(async () => {
+    await ThreadsTableTestHelper.cleanTable();
+    await UsersTableTestHelper.cleanTable();
+  });
+
+  beforeEach(async () => {
+    await UsersTableTestHelper.addUser({
+      id: 'user-123',
+      username: 'dicoding',
+      password: 'secret',
+      fullname: 'Dicoding Indonesia',
+    });
+  });
+
+  describe('when POST /threads', () => {
+    it('should response 201 and persisted thread', async () => {
+      // Arrange
+      const requestPayload = {
+        title: 'title',
+        body: 'body',
+      };
+      // eslint-disable-next-line no-undef
+      const server = await createServer(container);
+      const { accessToken } = await AuthenticationsTestHelper.getAccessToken(server);
+
+      // Action
+      const response = await server.inject({
+        method: 'POST',
+        url: '/threads',
+        payload: requestPayload,
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+
+      // Assert
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(201);
+      expect(responseJson.status).toEqual('success');
+      expect(responseJson.data.addedThread).toBeDefined();
+    });
+
+    it('should response 400 when request payload not contain needed property', async () => {
+      // Arrange
+      const requestPayload = {
+        title: 'title',
+      };
+      const server = await createServer(container);
+      const { accessToken } = await AuthenticationsTestHelper.getAccessToken(server);
+
+      // Action
+      const response = await server.inject({
+        method: 'POST',
+        url: '/threads',
+        payload: requestPayload,
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+
+      // Assert
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(400);
+      expect(responseJson.status).toEqual('fail');
+      expect(responseJson.message).toEqual('tidak dapat membuat thread baru karena properti yang dibutuhkan tidak ada');
+    });
+
+    it('should response 400 when request payload not meet data type specification', async () => {
+      // Arrange
+      const requestPayload = {
+        title: 'title',
+        body: ['body'],
+      };
+      const server = await createServer(container);
+      const { accessToken } = await AuthenticationsTestHelper.getAccessToken(server);
+
+      // Action
+      const response = await server.inject({
+        method: 'POST',
+        url: '/threads',
+        payload: requestPayload,
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+
+      // Assert
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(400);
+      expect(responseJson.status).toEqual('fail');
+      expect(responseJson.message).toEqual('tidak dapat membuat thread baru karena tipe data tidak sesuai');
+    });
+  });
+});
