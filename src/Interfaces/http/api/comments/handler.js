277f27080e657d67f6225d4f593002780d911444
Sultan Fariz <sultan_fariz@apps.ipb.ac.id>
2022-05-03 21:39:53 +0700

    feat: add route to delete comment

--- src/Interfaces/http/api/comments/handler.js
+++ src/Interfaces/http/api/comments/handler.js
@@ -1,10 +1,12 @@
 const AddCommentUseCase = require('../../../../Applications/use_case/AddCommentUseCase');
+const DeleteCommentUseCase = require('../../../../Applications/use_case/DeleteCommentUseCase');
 
 class CommentsHandler {
   constructor(container) {
     this._container = container;
 
     this.postCommentHandler = this.postCommentHandler.bind(this);
+    this.deleteCommentHandler = this.deleteCommentHandler.bind(this);
   }
 
   async postCommentHandler(request, h) {
@@ -26,6 +28,19 @@ class CommentsHandler {
       },
     });
     response.code(201);
+    return response;
+  }
+
+  async deleteCommentHandler(request, h) {
+    const { threadId: thread_id, commentId: comment_id } = request.params;
+    const { id: owner_id } = request.auth.credentials;
+    const deleteCommentUseCase = this._container.getInstance(DeleteCommentUseCase.name);
+    await deleteCommentUseCase.execute({ thread_id, comment_id, owner_id });
+
+    const response = h.response({
+      status: 'success',
+    });
+    response.code(200);
     return response;
   }
 }
--- src/Interfaces/http/api/comments/routes.js
+++ src/Interfaces/http/api/comments/routes.js
@@ -9,6 +9,16 @@ const routes = (handler) => ([
       description: 'Create a new comment',
     },
   },
+  {
+    method: 'DELETE',
+    path: '/threads/{threadId}/comments/{commentId}',
+    handler: handler.deleteCommentHandler,
+    options: {
+      auth: 'forum_api_jwt',
+      tags: ['api', 'comments'],
+      description: 'Delete a comment',
+    },
+  }
 ]);
 
 module.exports = routes;
