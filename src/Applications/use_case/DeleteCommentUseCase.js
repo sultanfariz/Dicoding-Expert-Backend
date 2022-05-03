6628bc1f93290d360064f90fffb491a95f46168d
Sultan Fariz <sultan_fariz@apps.ipb.ac.id>
2022-05-03 21:37:32 +0700

    feat: add authorization checker before delete comment

--- src/Applications/use_case/DeleteCommentUseCase.js
+++ src/Applications/use_case/DeleteCommentUseCase.js
@@ -1,13 +1,14 @@
 const NotFoundError = require('../../Commons/exceptions/NotFoundError');
+const AuthorizationError = require('../../Commons/exceptions/AuthorizationError');
 
 class DeleteCommentUseCase {
-  constructor({ commentRepository, threadRepository }) {
+  constructor({ commentRepository, threadRepository, userRepository }) {
     this._commentRepository = commentRepository;
     this._threadRepository = threadRepository;
+    this._userRepository = userRepository;
   }
 
   async execute(useCasePayload) {
-    console.log('DeleteCommentUseCase', useCasePayload);
     const thread = await this._threadRepository.getThreadById(useCasePayload.thread_id);
     if (!thread) {
       throw new NotFoundError('thread not found');
@@ -16,6 +17,13 @@ class DeleteCommentUseCase {
     if (!useCasePayload.comment_id) {
       throw new NotFoundError('comment_id is required');
     }
+
+    // check user authorization
+    const comment = await this._commentRepository.getCommentById(useCasePayload.comment_id);
+    if (!comment) throw new NotFoundError('comment not found');
+
+    const username = await this._userRepository.verifyUsernameById(useCasePayload.owner_id);
+    if (comment.username !== username) throw new AuthorizationError('user not authorized');
 
     return await this._commentRepository.deleteComment(useCasePayload.comment_id);
   }
--- src/Applications/use_case/_test/DeleteCommentUseCase.test.js
+++ src/Applications/use_case/_test/DeleteCommentUseCase.test.js
@@ -1,20 +1,25 @@
 const CommentRepository = require('../../../Domains/comments/CommentRepository');
 const ThreadRepository = require('../../../Domains/threads/ThreadRepository');
+const UserRepository = require('../../../Domains/users/UserRepository');
 const DeleteCommentUseCase = require('../DeleteCommentUseCase');
 
 describe('DeleteCommentUseCase', () => {
   it('should throw error when thread_id is not provided', async () => {
     const useCasePayload = {
       comment_id: 'comment-123',
+      user_id: 'user-123',
     };
     const mockThreadRepository = new ThreadRepository();
     mockThreadRepository.getThreadById = jest.fn().mockReturnValue(Promise.resolve(null));
     const mockCommentRepository = new CommentRepository();
     mockCommentRepository.deleteComment = jest.fn();
+    const mockUserRepository = new UserRepository();
+    mockUserRepository.getUsernameById = jest.fn().mockReturnValue(Promise.resolve('username'));
 
     const deleteCommentUseCase = new DeleteCommentUseCase({
       threadRepository: mockThreadRepository,
       commentRepository: mockCommentRepository,
+      userRepository: mockUserRepository,
     });
 
     // Act
@@ -28,11 +33,14 @@ describe('DeleteCommentUseCase', () => {
   it('should throw error when comment_id is not provided', async () => {
     const useCasePayload = {
       thread_id: 'thread-123',
+      user_id: 'user-123',
     };
     const mockThreadRepository = new ThreadRepository();
     mockThreadRepository.getThreadById = jest.fn().mockReturnValue(Promise.resolve({}));
     const mockCommentRepository = new CommentRepository();
     mockCommentRepository.deleteComment = jest.fn();
+    const mockUserRepository = new UserRepository();
+    mockUserRepository.getUsernameById = jest.fn().mockReturnValue(Promise.resolve('username'));
 
     const deleteCommentUseCase = new DeleteCommentUseCase({
       threadRepository: mockThreadRepository,
@@ -52,6 +60,7 @@ describe('DeleteCommentUseCase', () => {
     const useCasePayload = {
       thread_id: 'thread-123',
       comment_id: 'comment-123',
+      user_id: 'user-123',
     };
     const mockThreadRepository = new ThreadRepository();
     mockThreadRepository.getThreadById = jest.fn()
@@ -60,14 +69,24 @@ describe('DeleteCommentUseCase', () => {
         title: 'thread-123',
         content: 'thread-123',
         created_at: '2020-01-01',
-        username: 'user-123',
+        username: 'username',
       });
     const mockCommentRepository = new CommentRepository();
+    mockCommentRepository.getCommentById = jest.fn()
+      .mockResolvedValue({
+        id: 'comment-123',
+        content: 'content',
+        created_at: '2020-01-01',
+        username: 'username',
+      });
     mockCommentRepository.deleteComment = jest.fn();
+    const mockUserRepository = new UserRepository();
+    mockUserRepository.verifyUsernameById = jest.fn().mockReturnValue(Promise.resolve('username'));
 
     const deleteCommentUseCase = new DeleteCommentUseCase({
       threadRepository: mockThreadRepository,
       commentRepository: mockCommentRepository,
+      userRepository: mockUserRepository,
     });
 
     // Act
