3f09da7ce0805a7bac6b9701484efe85d1a52767
Sultan Fariz <sultan_fariz@apps.ipb.ac.id>
2022-06-09 09:54:51 +0700

    test: add unit test for like repositories layer

---
+++ src/Infrastructures/repository/_test/LikeRepositoryPostgres.test.js
@@ -0,0 +1,111 @@
+const LikesTableTestHelper = require('../../../../tests/LikesTableTestHelper');
+const CommentsTableTestHelper = require('../../../../tests/CommentsTableTestHelper');
+const ThreadsTableTestHelper = require('../../../../tests/ThreadsTableTestHelper');
+const UsersTableTestHelper = require('../../../../tests/UsersTableTestHelper');
+const InvariantError = require('../../../Commons/exceptions/InvariantError');
+const NotFoundError = require('../../../Commons/exceptions/NotFoundError');
+const GiveLike = require('../../../Domains/likes/entities/GiveLike');
+const pool = require('../../database/postgres/pool');
+const LikeRepositoryPostgres = require('../LikeRepositoryPostgres');
+
+
+describe('LikeRepositoryPostgres', () => {
+  afterEach(async () => {
+    await LikesTableTestHelper.cleanTable();
+    await CommentsTableTestHelper.cleanTable();
+    await ThreadsTableTestHelper.cleanTable();
+    await UsersTableTestHelper.cleanTable();
+  });
+
+  afterAll(async () => {
+    await pool.end();
+  });
+
+  describe('likeComment function', () => {
+    it('should persist add comment and return added comment correctly', async () => {
+      // Arrange
+      await UsersTableTestHelper.addUser({
+        id: 'user-123',
+        username: 'dicoding',
+        password: 'secret',
+        fullname: 'Dicoding Indonesia',
+      });
+      await ThreadsTableTestHelper.insertThread({
+        id: 'thread-123',
+        title: 'Dicoding Indonesia',
+        body: 'All About Dicoding Indonesia here',
+        owner_id: 'user-123',
+      });
+      await CommentsTableTestHelper.insertComment({
+        id: 'comment-123',
+        content: 'Hello World',
+        owner_id: 'user-123',
+      });
+      const giveLike = new GiveLike({
+        owner_id: 'user-123',
+        comment_id: 'comment-123',
+      });
+      const fakeIdGenerator = () => '123'; // stub!
+      const likeRepositoryPostgres = new LikeRepositoryPostgres(pool, fakeIdGenerator);
+
+      // Action
+      await likeRepositoryPostgres.likeComment(giveLike);
+
+      // Assert
+      const like = await LikesTableTestHelper.findLikeById('like-123');
+      expect(like).toHaveLength(1);
+    });
+  });
+
+  describe('unlikeComment', () => {
+    it('should throw InvariantError when like id is not valid', async () => {
+      // Arrange
+      const fakeIdGenerator = () => '123'; // stub!
+      const likeRepositoryPostgres = new LikeRepositoryPostgres(pool, fakeIdGenerator);
+
+      // Action and Assert
+      await expect(likeRepositoryPostgres.unlikeComment('invalid-id')).rejects.toThrowError(InvariantError);
+    });
+
+    it('should delete like when like id is valid', async () => {
+      // Arrange
+      await UsersTableTestHelper.addUser({
+        id: 'user-123',
+        username: 'dicoding',
+        password: 'secret',
+        fullname: 'Dicoding Indonesia',
+      });
+
+      await ThreadsTableTestHelper.insertThread({
+        id: 'thread-123',
+        title: 'Dicoding Indonesia',
+        body: 'All About Dicoding Indonesia here',
+        owner_id: 'user-123',
+      });
+
+      await CommentsTableTestHelper.insertComment({
+        id: 'comment-123',
+        content: 'dicoding',
+        owner_id: 'user-123',
+        thread_id: 'thread-123',
+      });
+
+      await LikesTableTestHelper.insertLike({
+        id: 'like-123',
+        content: 'dicoding',
+        owner_id: 'user-123',
+        comment_id: 'comment-123',
+      });
+
+      const fakeIdGenerator = () => '123'; // stub!
+      const likeRepositoryPostgres = new LikeRepositoryPostgres(pool, fakeIdGenerator);
+
+      // Action
+      await likeRepositoryPostgres.unlikeComment('like-123');
+
+      // Assert
+      const replies = await LikesTableTestHelper.findLikeById('like-123');
+      expect(replies).toHaveLength(0);
+    });
+  });
+});
