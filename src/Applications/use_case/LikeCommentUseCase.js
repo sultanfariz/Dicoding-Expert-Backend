01fbe69e12c7b9b065d94af6125bde24fe88d4c4
Sultan Fariz <sultan_fariz@apps.ipb.ac.id>
2022-06-09 12:50:23 +0700

    feat: add like comment usecase layer

---
+++ src/Applications/use_case/LikeCommentUseCase.js
@@ -0,0 +1,19 @@
+const GiveLike = require('../../Domains/likes/entities/GiveLike');
+
+class LikeCommentUseCase {
+  constructor({ likeRepository, commentRepository, threadRepository }) {
+    this._likeRepository = likeRepository;
+    this._commentRepository = commentRepository;
+    this._threadRepository = threadRepository;
+  }
+
+  async execute(useCasePayload) {
+    const giveLike = new GiveLike(useCasePayload);
+    await this._threadRepository.getThreadById(useCasePayload.thread_id);
+    let comment = await this._commentRepository.getCommentById(giveLike.comment_id);
+    await this._likeRepository.likeComment(giveLike);
+    await this._commentRepository.updateCommentLikeCountById(giveLike.comment_id, comment.likeCount + 1);
+  }
+}
+
+module.exports = LikeCommentUseCase;
