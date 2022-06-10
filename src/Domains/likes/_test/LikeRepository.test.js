71ad0342100e640612d02db4aa6df533c31b825e
Sultan Fariz <sultan_fariz@apps.ipb.ac.id>
2022-06-10 19:43:09 +0700

    test: add testcases for get like by owner and comment repository

--- src/Domains/likes/_test/LikeRepository.test.js
+++ src/Domains/likes/_test/LikeRepository.test.js
@@ -6,7 +6,7 @@ describe('LikeRepository interface', () => {
     const likeRepository = new LikeRepository();
 
     // Action and Assert
-    await expect(likeRepository.likeComment()).rejects.toThrowError('REPLY_REPOSITORY.METHOD_NOT_IMPLEMENTED');
-    await expect(likeRepository.unlikeComment()).rejects.toThrowError('REPLY_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+    await expect(likeRepository.likeComment()).rejects.toThrowError('LIKE_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+    await expect(likeRepository.unlikeComment()).rejects.toThrowError('LIKE_REPOSITORY.METHOD_NOT_IMPLEMENTED');
   });
 });
--- src/Infrastructures/repository/_test/LikeRepositoryPostgres.test.js
+++ src/Infrastructures/repository/_test/LikeRepositoryPostgres.test.js
@@ -104,7 +104,6 @@ describe('LikeRepositoryPostgres', () => {
 
       await LikesTableTestHelper.insertLike({
         id: 'like-123',
-        content: 'dicoding',
         owner_id: 'user-123',
         comment_id: 'comment-123',
       });
@@ -118,6 +117,140 @@ describe('LikeRepositoryPostgres', () => {
       // Assert
       const replies = await LikesTableTestHelper.findLikeById('like-123');
       expect(replies).toHaveLength(0);
+    });
+  });
+
+  describe('getLikeByCommentIdAndOwnerId function', () => {
+    it('should throw InvariantError when comment id is empty', async () => {
+      // Arrange
+      const fakeIdGenerator = () => '123'; // stub!
+      const likeRepositoryPostgres = new LikeRepositoryPostgres(pool, fakeIdGenerator);
+
+      // Action and Assert
+      await expect(likeRepositoryPostgres.getLikeByCommentIdAndOwnerId('', 'owner-123')).rejects.toThrowError(InvariantError);
+    });
+
+    it('should throw InvariantError when owner id is empty', async () => {
+      // Arrange
+      const fakeIdGenerator = () => '123'; // stub!
+      const likeRepositoryPostgres = new LikeRepositoryPostgres(pool, fakeIdGenerator);
+
+      // Action and Assert
+      await expect(likeRepositoryPostgres.getLikeByCommentIdAndOwnerId('comment-123', '')).rejects.toThrowError(InvariantError);
+    });
+
+    it('should return empty array when comment id is not found', async () => {
+      // Arrange
+      await UsersTableTestHelper.addUser({
+        id: 'user-123',
+        username: 'dicoding',
+        password: 'secret',
+        fullname: 'Dicoding Indonesia',
+      });
+
+      await ThreadsTableTestHelper.insertThread({
+        id: 'thread-123',
+        title: 'Dicoding Indonesia',
+        body: 'All About Dicoding Indonesia here',
+        owner_id: 'user-123',
+      });
+
+      await CommentsTableTestHelper.insertComment({
+        id: 'comment-123',
+        content: 'dicoding',
+        owner_id: 'user-123',
+        thread_id: 'thread-123',
+      });
+
+      const fakeIdGenerator = () => '123'; // stub!
+      const likeRepositoryPostgres = new LikeRepositoryPostgres(pool, fakeIdGenerator);
+
+      // Action
+      const likes = await likeRepositoryPostgres.getLikeByCommentIdAndOwnerId('comment-124', 'owner-123');
+
+      // Assert
+      expect(likes).toBe(null);
+    });
+
+    it('should return null when owner id is not found', async () => {
+      // Arrange
+      await UsersTableTestHelper.addUser({
+        id: 'user-123',
+        username: 'dicoding',
+        password: 'secret',
+        fullname: 'Dicoding Indonesia',
+      });
+
+      await ThreadsTableTestHelper.insertThread({
+        id: 'thread-123',
+        title: 'Dicoding Indonesia',
+        body: 'All About Dicoding Indonesia here',
+        owner_id: 'user-123',
+      });
+
+      await CommentsTableTestHelper.insertComment({
+        id: 'comment-123',
+        content: 'dicoding',
+        owner_id: 'user-123',
+        thread_id: 'thread-123',
+      });
+
+      await LikesTableTestHelper.insertLike({
+        id: 'like-123',
+        owner_id: 'user-123',
+        comment_id: 'comment-123',
+      });
+
+      const fakeIdGenerator = () => '123'; // stub!
+      const likeRepositoryPostgres = new LikeRepositoryPostgres(pool, fakeIdGenerator);
+
+      // Action
+      const likes = await likeRepositoryPostgres.getLikeByCommentIdAndOwnerId('comment-123', 'owner-124');
+
+      // Assert
+      expect(likes).toBe(null);
+    });
+
+    it('should return like when comment id and owner id is valid', async () => {
+      // Arrange
+      await UsersTableTestHelper.addUser({
+        id: 'user-123',
+        username: 'dicoding',
+        password: 'secret',
+        fullname: 'Dicoding Indonesia',
+      });
+
+      await ThreadsTableTestHelper.insertThread({
+        id: 'thread-123',
+        title: 'Dicoding Indonesia',
+        body: 'All About Dicoding Indonesia here',
+        owner_id: 'user-123',
+      });
+
+      await CommentsTableTestHelper.insertComment({
+        id: 'comment-123',
+        content: 'dicoding',
+        owner_id: 'user-123',
+        thread_id: 'thread-123',
+      });
+
+      await LikesTableTestHelper.insertLike({
+        id: 'like-123',
+        owner_id: 'user-123',
+        comment_id: 'comment-123',
+      });
+
+      const fakeIdGenerator = () => '123'; // stub!
+      const likeRepositoryPostgres = new LikeRepositoryPostgres(pool, fakeIdGenerator);
+
+      // Action
+      const likes = await likeRepositoryPostgres.getLikeByCommentIdAndOwnerId('comment-123', 'user-123');
+
+      // Assert
+      expect(likes).toBeDefined();
+      expect(likes.id).toBe('like-123');
+      expect(likes.owner_id).toBe('user-123');
+      expect(likes.comment_id).toBe('comment-123');
     });
   });
 });
