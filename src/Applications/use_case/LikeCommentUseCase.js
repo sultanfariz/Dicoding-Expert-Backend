890b1f00d34824a56fcc6bf3a444334b382afbfe
Sultan Fariz <sultan_fariz@apps.ipb.ac.id>
2022-06-11 11:37:04 +0700

    chore: adjust usecase due to changes

--- src/Applications/use_case/LikeCommentUseCase.js
+++ src/Applications/use_case/LikeCommentUseCase.js
@@ -12,13 +12,8 @@ class LikeCommentUseCase {
     await this._threadRepository.getThreadById(useCasePayload.thread_id);
     let comment = await this._commentRepository.getCommentById(giveLike.comment_id);
     let like = await this._likeRepository.getLikeByCommentIdAndOwnerId(giveLike.comment_id, giveLike.owner_id);
-    if (!like) {
-      await this._likeRepository.likeComment(giveLike);
-      await this._commentRepository.updateCommentLikeCountById(giveLike.comment_id, comment.likeCount + 1);
-    } else {
-      await this._likeRepository.unlikeComment(like.id);
-      await this._commentRepository.updateCommentLikeCountById(giveLike.comment_id, comment.likeCount - 1);
-    }
+    await this._likeRepository.likeComment(giveLike);
+    await this._commentRepository.updateCommentLikeCountById(giveLike.comment_id, comment.likeCount + 1);
   }
 }
 
